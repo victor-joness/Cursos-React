fbf59acc49e213fbb6a42ede8b8ba571aeada292
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-03-26 20:13:19 -0300

    Create App.jsx

---
+++ Curso-React-Vinicius-Dacal/Dia 01/my-app/src/App.jsx
@@ -0,0 +1,45 @@
+import React , { Component } from 'react';
+import logo from './img/logo.svg';
+import './styles/App.css';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      showImage: false,
+    }
+  }
+
+  toggle = () => {
+    this.setState({
+      showImage: !this.state.showImage
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button type='button' onClick={this.toggle}>{this.state.showImage ? "Mostrar" : "Esconder"}</button>
+          {(this.state.showImage === false) && (
+            <img src={logo} className="App-logo" alt="logo" />
+          )}
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
