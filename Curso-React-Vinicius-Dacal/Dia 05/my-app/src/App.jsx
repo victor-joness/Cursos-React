40e8a0867bc3d2d55a2e47c72f2ee451c77343f3
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-04-16 10:35:19 -0300

    Create App.jsx

---
+++ Curso-React-Vinicius-Dacal/Dia 05/my-app/src/App.jsx
@@ -0,0 +1,28 @@
+import PromotionCard from './Components/Promotion/Card/Card';
+
+
+const App = () => {
+  
+  const promotion = {
+    "id": 1,
+    "title": "Kit Notebook Acer Aspire 3 + Mochila Green, A315-41-R790, AMD Ryzen 3 2200U Dual Core",
+    "price": 1799,
+    "imageUrl": "https://cdn.gatry.com/gatry-static/promocao/imagem/76f53c235d5cd07c007ac8f4a6820e76.jpg",
+    "url": "https://www.amazon.com.br/Notebook-Acer-Mochila-A315-41-R790-Mem%C3%B3ria/dp/B07YDWLV7S/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=gatry0b-20&linkId=e4a1146599e36741a720a6a952cbc328&language=pt_BR",
+    "comments": [
+      {
+        "id": 1,
+        "comment": "TELA HD"
+      }
+    ]
+  };
+
+
+  return (
+    <div className="App" style={{maxWidth:800, margin:"30px auto"}}>
+      <PromotionCard promotion={promotion}/>
+    </div>
+  );
+}
+
+export default App;
